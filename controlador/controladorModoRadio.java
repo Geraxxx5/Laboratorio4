738f00157786a5b0e8b95194404db54b4dbc461c
Geraxxx5 <gerax.pineda@gmail.com>
2022-11-18 21:48:20 -0600

    Se hicieron los primeros 4 puntos

--- controlador/controladorModoRadio.java
+++ controlador/controladorModoRadio.java
@@ -10,7 +10,7 @@ import modelo.IVistaRadio;
 import modelo.canciones;
 import modelo.modeloRadio;
 
-public class controladorModoRadio implements IVistaRadio, IRadio, ITelefono{
+public class controladorModoRadio implements IVistaRadio, IRadio{
 
     modeloRadio mR = new modeloRadio();
 
@@ -18,34 +18,73 @@ public class controladorModoRadio implements IVistaRadio, IRadio, ITelefono{
         this.mR = mR;
     }
 
+    public void power(){
+        if(mR.isPower()){
+            mR.setPower(false);
+        }else{
+            mR.setPower(true);
+        }
+    }
+
+
+    public void subirVolumen(){
+        if(mR.getVolumen() != 40){
+            mR.setVolumen(mR.getVolumen() +1);
+        }
+    }
+
+    public void bajarVolumen(){
+        if(mR.getVolumen() != 0){
+            mR.setVolumen(mR.getVolumen() -1);
+        }
+    }
+
+    public void cambiarAModoRadio(){
+        mR.setModoReproduccion(false);
+        //mR.setModoTelefono(false);
+        Encender();
+    }
+    
+
     @Override
-    public void conectarTelefono() {
+    public void Encender() {
+        mR.setModoRadio(true);
+    }
+
+    
+    @Override
+    public void Apagar() {
+        mR.setModoRadio(false);
+    }
+
+
+    @Override
+    public void cambiarEmisora() {
         // TODO Auto-generated method stub
-        
+        if(mR.getEmisora() != 50){
+            mR.setEmisora(mR.getEmisora()+0.5);
+        }
+    }
+
+    public void bajarEmisora(){
+        if(mR.getEmisora() != 0){
+            mR.setEmisora(mR.getEmisora()-0.5);
+        }
     }
 
     @Override
-    public void mostrarContactos(Array contactos) {
-        // TODO Auto-generated method stub
-        
+    public double guardarEmisora() {
+        mR.getEmisorasGuardadas().add(mR.getEmisora());
+        return 0;
     }
 
     @Override
-    public void LlamarContacto(int contacto) {
-        // TODO Auto-generated method stub
+    public void cargarEmisora() {
         
     }
 
-    @Override
-    public void FinalizarLlamada() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void llamarUltimoContacto(int ultimoContacto) {
-        // TODO Auto-generated method stub
-        
+    public void cargarLaEmisora(int pos){
+        mR.setEmisora(mR.getEmisorasGuardadas().get(pos));
     }
 
     @Override
@@ -54,34 +93,6 @@ public class controladorModoRadio implements IVistaRadio, IRadio, ITelefono{
         
     }
 
-    @Override
-    public void cambiarEmisora() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public double guardarEmisora() {
-        // TODO Auto-generated method stub
-        return 0;
-    }
-
-    @Override
-    public void cargarEmisora() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void Encender() {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void Apagar() {
-        // TODO Auto-generated method stub
-        
-    }
+    
  
 }
--- radio/RadioMain.java
+++ radio/RadioMain.java
@@ -14,9 +14,101 @@ public class RadioMain{
         boolean repetir = true;
         //----------------------------------------------//
         while(repetir){
-            
+            System.out.println("El carro actualemnte esta: "+encendidoApagado(mR.isPower()));
+            if(mR.isPower()){
+                System.out.println("Volumen Actual: "+mR.getVolumen());
+                if(mR.isModoRadio()){
+                    System.out.println("Estado radio: "+mR.getEstadoRadio());
+                    System.out.println("Emisora Actual: "+mR.getEmisora());
+                }
+            }
+            System.out.println("1) Salir\n2) Encender/Apagar\n3) Cambiar Volumen"+
+                                "\n4) Modo Radio");
+            System.out.println("Seleccione una opcion");
+            String menu = sc.nextLine();
+            if(menu.equals("1")){
+                repetir = false;
+            }
+            if(menu.equals("2")){
+                cmr.power();
+            }
+            if(menu.equals("3")){
+                if(mR.isPower()){
+                    System.out.println("1) Subir Volumen\n2) Bajar Volumen\nSeleccione la opcion:");
+                    menu = sc.nextLine();
+                    if(menu.equals("1")){
+                        cmr.subirVolumen();
+                    }else{
+                        cmr.bajarVolumen();
+                    }
+                }else{
+                    System.out.println("Se necesita encender el carro primero");
+                }
+            }
+            if(menu.equals("4")){
+                if(mR.isPower()){
+                    cmr.cambiarAModoRadio();
+                    boolean modoRadio = true;
+                    while(modoRadio){
+                        System.out.println("Estado radio: "+mR.getEstadoRadio());
+                        System.out.println("Emisora Actual: "+mR.getEmisora());
+                        System.out.println("1) Salir del modo radio\n2) Cambiar emisora\n3) Guardar emisora\n4) Cargar Emisora");
+                        System.out.println("Seleccione la opcion que desea: ");
+                        menu = sc.nextLine();
+                        if(menu.equals("1")){
+                            modoRadio = false;
+                        }
+                        if(menu.equals("2")){
+                            System.out.println("1) Subir emisora\n2) Bajar emisora\nSeleccione la opcion:");
+                            menu = sc.nextLine();
+                            if(menu.equals("1")){
+                                cmr.cambiarEmisora();
+                            }else{
+                                cmr.bajarEmisora();
+                            }
+                        }
+                        if(menu.equals("3")){
+                            if(mR.getEmisorasGuardadas().size() != 50){
+                                System.out.println("Se guardo la emisora");
+                                cmr.guardarEmisora();
+                            }else{
+                                System.out.println("Ya no tiene espacio para guardar");
+                            }
+                        }
+                        if(menu.equals("4")){
+                            if(mR.getEmisorasGuardadas().size() != 0){
+                                for (int i = 0; i < (mR.getEmisorasGuardadas().size()); i++) {
+                                    System.out.println(i+") "+mR.getEmisorasGuardadas().get(i));
+                                }
+                                System.out.println("Seleccione la emisora que desea");
+                                try {
+                                    int opc = sc.nextInt();
+                                    sc.nextLine();
+                                    if(opc >= 0 && opc < mR.getEmisorasGuardadas().size()){
+                                        cmr.cargarLaEmisora(opc);
+                                    }
+                                } catch (Exception e) {
+                                    System.out.println("No selecciono ninguna emisora");
+                                }
+                            }else{
+                                System.out.println("No hay emisoras guardadas");
+                            }
+                        }
+
+                    }
+                }else{
+                    System.out.println("Se necesita encender el carro primero");
+                }
+            }
         }
 
+    }
+
+    public static String encendidoApagado(boolean power){
+        if(power){
+            return "Encendido";
+        }
+        return "Apagado";
     }
 
 }
