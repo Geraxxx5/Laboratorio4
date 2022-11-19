072da2e345045bad11343a3333cb0f2a7f876ec5
Brandon935 <95979416+Brandon935@users.noreply.github.com>
2022-11-19 09:02:02 -0600

    Se termino el programa
    Se agrego de 5 a 7

--- controlador/controladorModoRadio.java
+++ controlador/controladorModoRadio.java
@@ -10,7 +10,7 @@ import modelo.IVistaRadio;
 import modelo.canciones;
 import modelo.modeloRadio;
 
-public class controladorModoRadio implements IVistaRadio, IRadio{
+public class controladorModoRadio implements IVistaRadio, IRadio, ITelefono{
 
     modeloRadio mR = new modeloRadio();
 
@@ -26,6 +26,22 @@ public class controladorModoRadio implements IVistaRadio, IRadio{
         }
     }
 
+    public void cancionAdelante(){
+        if(mR.getPosCancion() < mR.getCanciones().get(mR.getPosReproduccion()).getCancion().size()){
+            mR.setPosCancion(mR.getPosCancion()+1);
+        }
+    }
+
+    public void cancionAtras(){
+        if(mR.getPosCancion() != 0){
+            mR.setPosCancion(mR.getPosCancion()-1);
+        }
+    }
+
+    public String escucharCancion(){
+        canciones c = mR.getCanciones().get(mR.getPosReproduccion()).getCancion().get(mR.getPosCancion());
+        return "Nombre: "+c.getNombre()+", duracion: "+c.getTiempo()+", autor: "+c.getAutor()+", genero: "+c.getGenero();
+    }
 
     public void subirVolumen(){
         if(mR.getVolumen() != 40){
@@ -45,6 +61,11 @@ public class controladorModoRadio implements IVistaRadio, IRadio{
         Encender();
     }
     
+    public void cambiarAmodoReproducir(){
+        Apagar();
+        //mR.setModoTelefono(false);
+        mR.setModoReproduccion(true);
+    }
 
     @Override
     public void Encender() {
@@ -93,6 +114,38 @@ public class controladorModoRadio implements IVistaRadio, IRadio{
         
     }
 
-    
- 
+    @Override
+    public void conectarTelefono() {
+        if(mR.isConectar()){
+            mR.setConectar(false);
+        }else{
+            mR.setConectar(true);
+        }
+        
+    }
+
+    @Override
+    public void mostrarContactos(Array contactos) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    @Override
+    public void LlamarContacto(int contacto) {
+        mR.setPosContacto(contacto);
+        mR.setEnLlamada(true);  
+    }
+
+    @Override
+    public void FinalizarLlamada() {
+        mR.setEnLlamada(false);
+        
+    }
+
+    @Override
+    public void llamarUltimoContacto(int ultimoContacto) {
+        mR.setPosContacto(ultimoContacto);
+        mR.setEnLlamada(true);  
+    }
+
 }
--- radio/RadioMain.java
+++ radio/RadioMain.java
@@ -21,9 +21,19 @@ public class RadioMain{
                     System.out.println("Estado radio: "+mR.getEstadoRadio());
                     System.out.println("Emisora Actual: "+mR.getEmisora());
                 }
+                if(mR.isModoReproduccion()){
+                    System.out.println("Reproductora: "+mR.getCanciones().get(mR.getPosReproduccion()).getNombre());
+                    System.out.println("Cancion actual: "+mR.getCanciones().get(mR.getPosReproduccion()).getCancion().get(mR.getPosCancion()).getNombre());
+                }
+                if(mR.isConectar()){
+                    System.out.println("Estado celular: "+encendidoApagado(mR.isConectar()));
+                    if(mR.isEnLlamada()){
+                        System.out.println("Hablando con: "+mR.getContactos().get(mR.getPosContacto()).getNombre());
+                    }
+                }
             }
             System.out.println("1) Salir\n2) Encender/Apagar\n3) Cambiar Volumen"+
-                                "\n4) Modo Radio");
+                                "\n4) Modo Radio\n5) Modo Reproduccion\n6) Modo Telefono\n7) Tarjetas de presentacion");
             System.out.println("Seleccione una opcion");
             String menu = sc.nextLine();
             if(menu.equals("1")){
@@ -100,6 +110,123 @@ public class RadioMain{
                     System.out.println("Se necesita encender el carro primero");
                 }
             }
+            if(menu.equals("5")){
+                if(mR.isPower()){
+                    cmr.cambiarAmodoReproducir();
+                    System.out.println("Que desea reproducir\n1) CD\n2) MP3\n3) Spotify");
+                    menu = sc.nextLine();
+                    if(menu.equals("1")){
+                        mR.setPosReproduccion(2);
+                    }else if(menu.equals("2")){
+                        mR.setPosReproduccion(1);
+                    }else{
+                        mR.setPosReproduccion(0);
+                    }
+                    boolean reproducir = true;
+                    mR.setPosCancion(0);
+                    while(reproducir){
+                        System.out.println("Reproductora: "+mR.getCanciones().get(mR.getPosReproduccion()).getNombre());
+                        System.out.println("Cancion actual: "+mR.getCanciones().get(mR.getPosReproduccion()).getCancion().get(mR.getPosCancion()).getNombre());
+                        System.out.println("Que desea realizar\n1) Salir modo Reproducir\n2) Cambiar Cancion\n3) Escuchar Cancion\nSeleccione una opcion");
+                        menu = sc.nextLine();
+                        if(menu.equals("1")){
+                            reproducir = false;
+                        }
+                        if(menu.equals("2")){
+                            System.out.println("1) Cancion para adelante\n2) Cancion para atras\nSeleccione la opcion:");
+                            menu = sc.nextLine();
+                            if(menu.equals("1")){
+                                //cmr.cambiarEmisora();
+                                cmr.cancionAdelante();
+                            }else{
+                                //cmr.bajarEmisora();
+                                cmr.cancionAtras();
+                            }
+                        }
+                        if(menu.equals("3")){
+                            System.out.println(cmr.escucharCancion());
+                        }
+
+                    }
+                }else{
+                    System.out.println("Se necesita encender el carro primero");
+                }
+            }
+            if(menu.equals("6")){
+                if(mR.isPower()){
+                    boolean modoTelefono = true;
+                    while(modoTelefono){
+                        System.out.println("Estado celular: "+encendidoApagado(mR.isConectar()));
+                        if(mR.isEnLlamada()){
+                            System.out.println("Hablando con: "+mR.getContactos().get(mR.getPosContacto()).getNombre());
+                        }
+                        System.out.println("1) Salir modo Telefono\n2) Conectar/Desconectar\n3) Mostrar Contactos\n4) Llamar a contacto\n5) Finalizar llamada"+
+                                        "\n6) Llamar a ultimo contacto con el que hablo");
+                        menu = sc.nextLine();
+                        if(menu.equals("1")){
+                            modoTelefono = false;
+                        }
+                        if(menu.equals("2")){
+                            mR.setModoRadio(false);
+                            mR.setModoReproduccion(false);
+                            cmr.conectarTelefono();
+                        }
+                        if(menu.equals("3")){
+                            if(mR.isConectar()){
+                                mostarContactos(mR);
+                            }else{
+                                System.out.println("Conecta el telefono antes");
+                            }
+                        }
+                        if(menu.equals("4")){
+                            if(mR.isConectar()){
+                                mostarContactos(mR);
+                                System.out.println("Seleccione la opcion que desea: ");
+                                try {
+                                    int opc = sc.nextInt();
+                                    sc.nextLine();
+                                    if(opc >= 0 && opc < mR.getContactos().size()){
+                                        cmr.LlamarContacto(opc);
+                                    }else{
+                                        System.out.println("No selecciono ninguna opcion");
+                                    }
+                                } catch (Exception e) {
+                                    System.out.println("No selecciono ninguna opcion");
+                                }
+                            }else{
+                                System.out.println("Conecta el telefono antes");
+                            }
+                        }
+                        if(menu.equals("5")){
+                            if(mR.isConectar()){
+                                mR.setEnLlamada(false);
+                            }else{
+                                System.out.println("Conecta el telefono antes");
+                            }
+                        }
+                        if(menu.equals("6")){
+                            if(mR.isConectar()){
+                                if(mR.getPosContacto() != -1){
+                                    cmr.FinalizarLlamada();
+                                }else{
+                                    System.out.println("No hay ultimo contacto");
+                                }
+                            }else{
+                                System.out.println("Conecta el telefono antes");
+                            }
+                        }
+                    }   
+                }else{
+                    System.out.println("Se necesita encender el carro primero");
+                }
+            }
+            if(menu.equals("7")){
+                if(mR.isPower()){
+                    System.out.println(mR.getTarjetas());
+                }else{
+                    System.out.println("Se necesita encender el carro primero");
+                }
+            }
         }
 
     }
@@ -109,6 +236,19 @@ public class RadioMain{
             return "Encendido";
         }
         return "Apagado";
+    }
+
+    public static String encendidoApagadoT(boolean power){
+        if(power){
+            return "Encendido";
+        }
+        return "Apagado";
+    }
+
+    public static void mostarContactos(modeloRadio mR){
+        for (int i = 0; i < mR.getContactos().size(); i++) {
+            System.out.println(i+") "+mR.getContactos().get(i).getNombre());
+        }
     }
 
 }
